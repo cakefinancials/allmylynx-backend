f740a395626ce4f7511957c57e113a0663878da1
Samuel Perez <samuelp@socialnative.com>
2018-05-30 09:53:03 -0700

    refactor the tests some more

--- test/server/services/user_state_bag.test.js
+++ test/server/services/user_state_bag.test.js
@@ -8,29 +8,37 @@ const buildTestBottle = testBottleBuilderFactory();
 describe("user_state_bag", () => {
     const testUserId = "someUser123";
     const testUserState = { some: "body" };
-    let bottle;
 
-    const getUserStateBagService = () => bottle.container[BOTTLE_NAMES.SERVICE_USER_STATE_BAG];
+    let awsClient;
+    let s3KeyGeneratorService;
+    let userStateBagService;
+
+    const buildUserStateBagTestBottle = (overrides) => {
+        const {bottle} = buildTestBottle(overrides);
+        awsClient = bottle.container[BOTTLE_NAMES.CLIENT_AWS];
+        s3KeyGeneratorService = bottle.container[BOTTLE_NAMES.SERVICE_S3_KEY_GENERATOR];
+        userStateBagService = bottle.container[BOTTLE_NAMES.SERVICE_USER_STATE_BAG];
+    };
 
     describe("writeUserState", () => {
         describe("when the save succeeds", () => {
             before(() => {
-                ({bottle} = buildTestBottle({
+                buildUserStateBagTestBottle({
                     [BOTTLE_NAMES.CLIENT_AWS]: {
                         s3PutObject: simple.stub().resolveWith('success')
                     }
-                }));
+                });
             });
 
             it("should return success true", async () => {
-                const result = await getUserStateBagService().writeUserState(testUserId, testUserState);
+                const result = await userStateBagService.writeUserState(testUserId, testUserState);
                 expect(result).to.deep.equal({success: true});
 
-                const s3PutObjectStub = bottle.container[BOTTLE_NAMES.CLIENT_AWS].s3PutObject;
+                const s3PutObjectStub = awsClient.s3PutObject;
                 expect(s3PutObjectStub.callCount).to.equal(1);
                 expect(s3PutObjectStub.lastCall.args).to.deep.equal([
                     TEST_ENV_VARS.USER_DATA_BUCKET,
-                    bottle.container[BOTTLE_NAMES.SERVICE_S3_KEY_GENERATOR].getUserStateBagKey(testUserId),
+                    s3KeyGeneratorService.getUserStateBagKey(testUserId),
                     JSON.stringify(testUserState)
                 ]);
             });
@@ -40,17 +48,18 @@ describe("user_state_bag", () => {
             const rejectErr = new Error("S3 PUT FAILURE");
 
             before(() => {
-                ({bottle} = buildTestBottle({
+                buildUserStateBagTestBottle({
                     [BOTTLE_NAMES.CLIENT_AWS]: {
                         s3PutObject: simple.stub().rejectWith(rejectErr)
                     }
-                }));
+                });
             });
 
             it("should throw the error", async () => {
-                const promiseToTest = getUserStateBagService().writeUserState(testUserId, testUserState);
+                const promiseToTest = userStateBagService.writeUserState(testUserId, testUserState);
                 const thrownErr = await promiseToTest.catch(e => e);
                 expect(thrownErr.nested).to.equal(rejectErr);
+                expect(thrownErr.message).to.equal(userStateBagService.CONSTANTS.WRITE_USER_STATE_FAILURE_MESSAGE);
             });
         });
     });
@@ -61,24 +70,24 @@ describe("user_state_bag", () => {
 
         describe("when the read succeeds", () => {
             before(() => {
-                ({bottle} = buildTestBottle({
+                buildUserStateBagTestBottle({
                     [BOTTLE_NAMES.CLIENT_AWS]: {
                         s3GetObject: simple.stub().resolveWith({
                             Body: JSON.stringify(testUserStateBag)
                         })
                     }
-                }));
+                });
             });
 
             it("should return the user state bag", async () => {
-                const result = await getUserStateBagService().readUserState(testUserId);
+                const result = await userStateBagService.readUserState(testUserId);
                 expect(result).to.deep.equal(testUserStateBag);
 
-                const s3GetObjectStub = bottle.container[BOTTLE_NAMES.CLIENT_AWS].s3GetObject;
+                const s3GetObjectStub = awsClient.s3GetObject;
                 expect(s3GetObjectStub.callCount).to.equal(1);
                 expect(s3GetObjectStub.lastCall.args).to.deep.equal([
                     TEST_ENV_VARS.USER_DATA_BUCKET,
-                    bottle.container[BOTTLE_NAMES.SERVICE_S3_KEY_GENERATOR].getUserStateBagKey(testUserId)
+                    s3KeyGeneratorService.getUserStateBagKey(testUserId)
                 ]);
             });
         });
@@ -87,17 +96,18 @@ describe("user_state_bag", () => {
             const rejectErr = new Error("S3 GET FAILURE");
 
             before(() => {
-                ({bottle} = buildTestBottle({
+                buildUserStateBagTestBottle({
                     [BOTTLE_NAMES.CLIENT_AWS]: {
                         s3GetObject: simple.stub().rejectWith(rejectErr)
                     }
-                }));
+                });
             });
 
             it("should throw the error", async () => {
-                const promiseToTest = getUserStateBagService().readUserState(testUserId);
+                const promiseToTest = userStateBagService.readUserState(testUserId);
                 const thrownErr = await promiseToTest.catch(e => e);
                 expect(thrownErr.nested).to.equal(rejectErr);
+                expect(thrownErr.message).to.equal(userStateBagService.CONSTANTS.READ_USER_STATE_FAILURE_MESSAGE);
             });
         });
 
@@ -106,15 +116,15 @@ describe("user_state_bag", () => {
             rejectErr.code = "NoSuchKey";
 
             before(() => {
-                ({bottle} = buildTestBottle({
+                buildUserStateBagTestBottle({
                     [BOTTLE_NAMES.CLIENT_AWS]: {
                         s3GetObject: simple.stub().rejectWith(rejectErr)
                     }
-                }));
+                });
             });
 
             it("should return an empty object", async () => {
-                const result = await getUserStateBagService().readUserState(testUserId);
+                const result = await userStateBagService.readUserState(testUserId);
                 expect(result).to.deep.equal({});
             });
         });
